a46e18e7c6e33212f8c5ed1d057a61e1e5e104ef
Uliana Myasischeva <ud.myasischeva@gmail.com>
2024-10-04 15:31:38 +0300

    fix/rootReducer

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -5,8 +5,8 @@ import { productsSlice } from './slices/products';
 import { usersSlice } from './slices/users';
 
 export const rootReducer = combineReducers({
-  [categoriesSlice.name]: categoriesSlice.reducer,
-  [orderSlice.name]: orderSlice.reducer,
-  [productsSlice.name]: productsSlice.reducer,
-  [usersSlice.name]: usersSlice.reducer
+  categories: categoriesSlice.reducer,
+  order: orderSlice.reducer,
+  products: productsSlice.reducer,
+  users: usersSlice.reducer
 });
