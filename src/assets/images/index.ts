e9599f5a3e127c4fcba47564402289afa471fef0
Anna Pavlova <icefly86@yandex.ru>
2024-10-11 22:38:18 +0300

    add carousel

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as vans } from './vans.png';
+export { default as newBalance } from './new-balance.png';
+export { default as nike } from './nike.png';
+export { default as porshe } from './porsche-design.png';
+export { default as puma } from './puma.png';
+export { default as reebok } from './reebok.png';
--- src/components/ui-kit/button/submitButton.tsx
+++ src/components/ui-kit/button/submitButton.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 import styles from './submitButton.module.scss';
+import { Button } from '@mui/material';
 
 // базовая кнопка для всех действий
 
-export const SubmitButton: FC = () => <></>;
+export const SubmitButton: FC = () => <>
+      <Button className={styles.submit} variant="contained">Подписаться</Button>
+      
+      </>;
