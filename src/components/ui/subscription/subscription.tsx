df27410c97616a1888c12c185a4ef80778c15d90
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2024-10-11 22:33:37 +0700

    add Subscription component

--- src/components/ui/subscription/subscription.tsx
+++ src/components/ui/subscription/subscription.tsx
@@ -1,11 +1,20 @@
 import { FC, memo } from 'react';
-import styles from './subscription.module.css';
+import styles from './subscription.module.scss';
 
 export const SubscriptionUI: FC = memo(() => (
-  <>
-    <>
-      {/* <TextField> </TextField> из material-ui
- <SubmitButton> из ui-kit */}
-    </>
-  </>
+  <div className={styles.container}>
+    <h3 className={styles.title}>
+      Подпишись на рассылку и получи скидку до 10 %{' '}
+    </h3>
+    <form
+      className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input type='text' placeholder='Введите ваш email' />
+      <button type='submit'>Подписаться</button>
+      <a href='#' className={styles.link}>Политика конфиденциальности</a>
+    </form>
+  </div>
 ));
