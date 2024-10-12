b2c2a063010f08466bd121b38709bfae77e49399
Anna Pavlova <icefly86@yandex.ru>
2024-10-12 22:27:21 +0300

    add card/advantages

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,4 @@
+export { default as box } from './box.svg';
+export { default as like } from './like.svg';
+export { default as help } from './help.svg';
+
--- src/components/ui-kit/index.ts
+++ src/components/ui-kit/index.ts
@@ -6,3 +6,4 @@ export * from './counter';
 export * from './logo';
 export * from './navigation';
 export * from './search';
+export * from './card';
