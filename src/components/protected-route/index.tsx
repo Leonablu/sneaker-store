6274284282207e796b05b052e09c3753fddfeffd
Leonablu <sergey.garbuzov.97@gmail.com>
2024-10-15 12:34:37 +0300

    add login/register page

--- src/components/protected-route/index.tsx
+++ src/components/protected-route/index.tsx
@@ -1,5 +1,5 @@
+import { FC, useState, useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useSelector } from '../../services/store';
 import { SkeletonUI } from '../ui/skeleton';
 
 type TProtectedRouteType = 'auth' | 'unauth';
@@ -9,14 +9,45 @@ type ProtectedRouteProps = {
   type: TProtectedRouteType;
 };
 
-export const ProtectedRoute = ({ children, type }: ProtectedRouteProps) => {
-  //   const { user, isAuthChecked, loading } = useSelector((state) => state.user);
-  const user = null;
-  const isAuthChecked = false;
-  const loading = false;
+// Функция, которая имитирует проверку авторизации пользователя
+const authenticateUser = async (email: string, password: string) =>
+  new Promise<{ isAuthenticated: boolean }>((resolve) => {
+    setTimeout(() => {
+      if (email === 'leona@mail.ru' && password === '123') {
+        resolve({ isAuthenticated: true });
+      } else {
+        resolve({ isAuthenticated: false });
+      }
+    }, 1000);
+  });
 
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({ children, type }) => {
+  const [user, setUser] = useState<null | {}>(null);
+  const [isAuthChecked, setIsAuthChecked] = useState(false);
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
   const from = location.state?.from || '/';
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const email = 'leona@mail.ru';
+      const password = '123';
+
+      try {
+        const { isAuthenticated } = await authenticateUser(email, password);
+        if (isAuthenticated) {
+          setUser({});
+        }
+      } catch (error) {
+        console.error('Ошибка проверки авторизации', error);
+      } finally {
+        setIsAuthChecked(true);
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
 
   if (!isAuthChecked || loading) {
     return <SkeletonUI />;
